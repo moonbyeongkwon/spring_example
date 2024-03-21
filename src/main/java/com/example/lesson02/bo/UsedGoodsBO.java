712756181416f5a5c3d5f68ed7fa3a771ece746d
test <test@test.com>
2024-03-21 21:42:46 +0900

    3/21 학원

---
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -0,0 +1,10 @@
+package com.example.lesson02.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsedGoodsBO {
+
+	// input: Controller로부터 받아온다
+	// output: Controller한테 준다.
+}
