268fc1f852c976475622f451cf3fe91007deb930
test <test@test.com>
2024-03-26 21:41:49 +0900

    3/26 학원

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -17,4 +17,21 @@ public class ReviewBO {
 	public Review getReview(int id) {
 		return reviewMapper.selectReview(id);
 	}
+	
+	// input: Review
+	// output: int(성공한 행의 개수)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	// addReviewAsField(4, "콤비네이션R", "바다", 5.0, "역시 맛있다!")
+	public int addReviewAsField(int storeId0, String menu0, String userName0, Double point0, String review0) {
+		return reviewMapper.insertReviewAsField(storeId0, menu0, userName0, point0, review0);
+	}
+	
+	//input: id, review
+	//output: int
+	public int updateReviewById(int id, String review) {
+		return reviewMapper.updateReviewById(id, review);
+	}
 }
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,11 +1,26 @@
 package com.example.lesson03.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.lesson03.domain.Review;
 
 @Mapper
 public interface ReviewMapper {
 	public Review selectReview(int id);
+	
+	public int insertReview(Review review);
+	
+	// @Param => 하나의 Map이 됨
+	public int insertReviewAsField(
+			@Param("storeId") int storeId11,
+			@Param("menu") String menu11,
+			@Param("userName") String userName11,
+			@Param("point") Double point11,
+			@Param("review") String review11);
+	
+	public int updateReviewById(
+			@Param("id") int id,
+			@Param("review") String review);
 }
 
