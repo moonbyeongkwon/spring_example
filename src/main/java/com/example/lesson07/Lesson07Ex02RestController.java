9b8b508d518a54d0ca39629faab4a010f2b94105
test <test@test.com>
2024-04-12 21:40:43 +0900

    4/12 학원

--- src/main/java/com/example/lesson07/Lesson07Ex02RestController.java
+++ src/main/java/com/example/lesson07/Lesson07Ex02RestController.java
@@ -50,4 +50,11 @@ public class Lesson07Ex02RestController {
 		return studentRepository.findByIdBetween(1, 2);
 		
 	}
+	
+	// native query
+	@GetMapping("/select2")
+	public List<StudentEntity> select2() {
+		// 장래희망이 래퍼인 데이터 조회
+		return studentRepository.findByDreamJob("래퍼");
+	}
 }
--- src/main/java/com/example/lesson07/repository/StudentRepository.java
+++ src/main/java/com/example/lesson07/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package com.example.lesson07.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.lesson07.entity.StudentEntity;
 
@@ -22,6 +24,14 @@ public interface StudentRepository extends JpaRepository<StudentEntity, Integer>
 	public List<StudentEntity> findByEmailContains(String keyword);
 	public List<StudentEntity> findByNameStartingWith(String keyword);
 	public List<StudentEntity> findByIdBetween(int startId, int endId);
+	
+	// ex02/2	native query
+	@Query(value = "select * from `new_student` where `dreamJob` =:dreamJob", nativeQuery = true) // native query(DB에 직접 조회)
+	public List<StudentEntity> findByDreamJob(@Param("dreamJob") String dreamJob);
+	
+	// ex02/2-2 JPQL(엔티티 조회)
+	//@Query(value = "select s from StudentEntity s where s.dreamJob =:dreamJob")
+	//public List<StudentEntity> findByDreamJob(@Param("dreamJob") String dreamJob);	
 	
 	
 }
